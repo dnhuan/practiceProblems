13055aaa0d6c713f56a1143e2e3a97eb550e13b5
dnhuan <huandngoc@gmail.com>
2020-10-30 01:43:26 +0700

    Solved integer factor

---
+++ Matlab/integerFactor.m
@@ -0,0 +1,9 @@
+% Integer factor
+disp('Integer factor')
+a = input('Enter first number: ');
+b = input('Enter second number: ');
+if(mod(a,b) == 0)
+    disp('Integer Factor')
+else
+    disp('Not Integer Factor')
+end
