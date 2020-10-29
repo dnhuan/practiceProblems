04e6f7d2c9d868686b8d28002bfc1297d19ce4f9
dnhuan <huandngoc@gmail.com>
2020-10-30 01:42:04 +0700

    Solved integer multiple

---
+++ Matlab/integerMultiple.m
@@ -0,0 +1,9 @@
+% Integer multiple
+disp('Integer multiple')
+a = input('Enter first number: ');
+b = input('Enter second number: ');
+if(mod(b,a) == 0)
+    disp('Integer Multiple')
+else
+    disp('Not Integer Multiple')
+end
